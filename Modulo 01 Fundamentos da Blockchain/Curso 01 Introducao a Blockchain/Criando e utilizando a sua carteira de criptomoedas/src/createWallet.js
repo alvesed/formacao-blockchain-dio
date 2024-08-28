ffce62051e7bd48c485b9d2cdc4ba84acbf3e31d
Edson Francisco Alves <alvesed@gmail.com>
2024-08-27 22:33:50 -0300

    Código para geração de carteiras para criptomoedas

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -5,29 +5,34 @@ const bitcoin = require('bitcoinjs-lib')
 
 //definir a rede
 //bitcoin - rede principal - mainnet
-//testnet - rede de teste - tesnet
+//testnet - rede de teste - testnet
 const network = bitcoin.networks.testnet
 
-//derivação de carteiras HD
-const path = `m/49'/1'/0'/0` 
+//derivacao de carteiras HD
+const path = `m/49'/1'/0'/0`
 
 //criando o mnemonic para a seed (palavras de senha)
 let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 
-//criando a raiz da cartiera HD
+//criando a raz da carteira HD
 let root = bip32.fromSeed(seed, network)
 
 //criando uma conta - par pvt-pub keys
 let account = root.derivePath(path)
 let node = account.derive(0).derive(0)
 
-let btcAddress = bitcoin.payments.p2pkh({
+// let btcAddress = bitcoin.payments.p2pkh({
+//     pubkey: node.publicKey,
+//     network: network,
+// }).address
+
+let bech32Address = bitcoin.payments.p2wpkh({
     pubkey: node.publicKey,
     network: network,
 }).address
 
 console.log("Carteira gerada")
-console.log("Endereço: ", btcAddress)
-console.log("Chave privada:", node.toWIF())
-console.log("Seed:", mnemonic)
+console.log("Endereço: ", bech32Address)
+console.log("Chave privada: ", node.toWIF())
+console.log("Seed: ", mnemonic)
